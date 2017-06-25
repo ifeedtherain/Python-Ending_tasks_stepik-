bc4858b62dea9b5919c61101c0904784c1cc399c
Daniel <ifeedtherain@gmail.com>
2017-06-25 19:36:06 +0300

    Collections for chek the max number of str that appers in file

---
+++ 25-06_IN_OUT.py
@@ -0,0 +1,9 @@
+import collections
+c = collections.Counter()
+inp = open('dataset_3363_3.txt', 'r')
+read_inp = inp.read().lower().split()
+for i in read_inp:
+	c[i] += 1
+ans = tuple(*(c.most_common(1)))
+print(read_inp)
+print(*ans)
