ba1708324bc89238d6826c6b3b4c015c96b6aac6
Risk of Storm <ifeedtherain@users.noreply.github.com>
2017-07-02 22:03:52 +0300

    Update 02-07_RegEx-3.py

--- 02-07_RegEx-3.py
+++ 02-07_RegEx-3.py
@@ -2,5 +2,5 @@ import sys
 import re
 for line in sys.stdin:
     line = line.rstrip()
-    if re.match(r'(z...z)+', line) != None:
+    if re.search(r'(z...z)+', line) != None:
         print.append(line)
