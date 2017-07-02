602dd174f093f2eb769df6ad9e75e217caccd6cd
Risk of Storm <ifeedtherain@users.noreply.github.com>
2017-07-02 12:49:05 +0300

    Add files via upload

--- fucking_namespaces.py
+++ fucking_namespaces.py
@@ -1,17 +1,51 @@
 dikt = dict()
 
 def add(n, v):
-	if n in dikt:
+	if n in dikt :
 		dikt[n].append(v)
+
 	else:
 		dikt[n]= [v]
 def create(n, p):
 	for k,v in dikt.items():
 		if k == p:
-			g = {n : 'None'}
-			dikt[k].append(dict(g))
+
+			dikt[k].append(n)
+def get(n, p):
+	for k, v in dikt.items():
+		if (n, p) == (k, [v]):
+			if k == p:
+				print(k)
+				return k
+
+
+def find_path(dikt, start, end, path= []):
+	path = path + [start]
+
+	if start == end:
+		return path
+
+	if start not in dikt.keys():
+		return None
+
+	for node in dikt[start]:
+
+		if node not in path:
+
+			newpath = find_path(dikt, node, end, path)
+
+			if newpath: return newpath
+
+	return None
 
 add('global', 'a')
 create('foo', 'global')
-
+add('foo', 'b')
+get('foo', 'a')
+get('foo', 'c')
+create('bar', 'foo')
+add('bar', 'a')
+get('bar', 'a')
+get('bar', 'b')
+print(find_path(dikt, 'a', 'bar'))
 print(dikt)
