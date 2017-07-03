74dd0ace92a30bcf802bbb3f2c4db498cf40b073
Risk of Storm <ifeedtherain@users.noreply.github.com>
2017-07-03 19:13:20 +0300

    Data analysis. The most high value of crime_type

---
+++ 03-07_CrimesChicago.py
@@ -0,0 +1,15 @@
+import csv
+
+crime = []
+ans = dict()
+with open('Crimes.csv') as f:
+    data = csv.DictReader(f)
+    for row in data:
+        if(row['Date'][6:10]) == '2005':
+            if row['Primary Type'] not in ans.keys():
+                ans[row['Primary Type']] = 1
+            elif row['Primary Type'] in ans.keys():
+                ans[row['Primary Type']] += 1
+for k, v in ans.items():
+    if k == max(ans, key= ans.get):
+        print(k,v)
