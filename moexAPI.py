df54d9cc5e61be2037a6886895612c8621ab3389
Risk of Storm <ifeedtherain@users.noreply.github.com>
2017-07-22 14:53:08 +0300

    fixed

---
+++ moexAPI.py
@@ -0,0 +1,21 @@
+import requests
+import zipfile
+import io
+import json
+
+mliquid = dict()
+
+url = 'http://moex.com/iss/downloads/statistics/engines/stock/monthly/latest.json.zip'
+res = requests.get(url)
+
+z = zipfile.ZipFile(io.BytesIO(res.content))
+z.extractall()
+z.close()
+
+with io.open('monthly_archive.json', 'r', encoding='utf-8')as f:
+    data_fl = json.load(f)
+for i in range(len(data_fl['monthly']["data"])):
+    if data_fl['monthly']["data"][i][8] >= 100000000000:
+        mliquid[data_fl['monthly']["data"][i][5]] = data_fl['monthly']["data"][i][8]
+print(mliquid)
+
